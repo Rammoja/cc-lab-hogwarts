a1c4c5fa1caaac40a7b6265d04b6dde77d9e4c0f
Colin Tindle <ampersand@underscore.space>
2018-05-03 11:26:37 +0100

    added find_all method fix a silly bug

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+require_relative('../models/Student')
+
+# student1 = Student.new({
+#   "first_name": "Barry",
+#   "second_name": "White",
+#   "house_name": "Hufflepuff",
+#   "age": 69
+# })
+
+# student1.save()
+
+# student2 = Student.new({
+#   "first_name": "Stephanie",
+#   "second_name": "Brown",
+#   "house_name": "Ravenclaw",
+#   "age": 35
+# })
+
+# student2.save()
+
+p Student.find_all()
--- models/student.rb
+++ models/student.rb
@@ -2,19 +2,28 @@ require_relative('../db/sql_runner')
 
 class Student
   attr_reader :id
-  attr_accessor :first_name, :last_name, :house_name, :age
+  attr_accessor :first_name, :second_name, :house_name, :age
 
   def initialize(options)
-    @id = options[:id].to_i if options[:id]
-    @first_name = options[:first_name]
-    @last_name = options[:last_name]
-    @house_name = options[:house_name]
-    @age = options[:age]
+    @id = options["id"].to_i if options["id"]
+    @first_name = options["first_name"]
+    @second_name = options["second_name"]
+    @house_name = options["house_name"]
+    @age = options["age"]
   end
 
   def save()
-    sql = "INSERT INTO students (first_name,last_name,house_name,age) VALUES ($1,$2,$3,$4) RETURNING id;"
-    values = [@first_name, @last_name, @house_name, @age]
-    @id = sql_runner.run(sql,values).first()['id'].to_i
+    sql = "INSERT INTO students (first_name,second_name,house_name,age) VALUES ($1,$2,$3,$4) RETURNING id;"
+    values = [@first_name, @second_name, @house_name, @age]
+    @id = SqlRunner.run(sql,values).first()['id'].to_i
+  end
+
+  def self.find_all()
+    students = SqlRunner.run("SELECT * FROM students;")
+    return self.map_items(students)
+  end
+
+  def self.map_items(items)
+    return items.map { |item| self.new(item) }
   end
 end
